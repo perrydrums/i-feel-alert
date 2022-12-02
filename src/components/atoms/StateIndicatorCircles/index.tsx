1c235be516a3a1e8d2dae678d02641f2fb533bd4
Perry Janssen <perry.janssen@iodigital.com>
2022-12-02 23:56:11 +0100

    feat: refactor stateIndicator

--- src/components/atoms/StateIndicatorCircles/index.tsx
+++ src/components/atoms/StateIndicatorCircles/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './style.css';
-import SoMStateTitle from "../text/SoMStateTitle";
 
 export default function StateIndicatorCircles({state}: {state: string}) {
   return (
@@ -8,11 +7,10 @@ export default function StateIndicatorCircles({state}: {state: string}) {
       <div className='state-indicator-circle sic--outer' style={{borderColor: `var(--color-border--${state}--25)`}}>
         <div className='state-indicator-circle sic--middle' style={{borderColor: `var(--color-border--${state}--50)`}}>
           <div className='state-indicator-circle sic--inner' style={{borderColor: `var(--color-border--${state})`}}>
-            <SoMStateTitle theme={state} />
+            <span className={`state-indicator-title theme--${state}`}>{state}</span>;
           </div>
         </div>
       </div>
     </div>
-
   );
 }
--- src/components/atoms/text/SoMStateTitle/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './style.css';
-
-export default function SoMStateTitle({theme}: {theme: string}) {
-  const themeClass = `theme--${theme}`;
-  return <span className={'state-color-title ' + themeClass}>{theme}</span>;
-}

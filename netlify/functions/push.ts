0c0190246af9750d7b4636e313fe1499a97a6e7f
Perry Janssen <perry.janssen@iodigital.com>
2023-08-18 15:06:14 +0200

    feat: add push function

--- netlify/functions/push.ts
+++ netlify/functions/push.ts
@@ -2,28 +2,28 @@ import { Handler } from "@netlify/functions";
 import * as OneSignal from "@onesignal/node-onesignal";
 
 export const handler: Handler = async (event, context) => {
-  const configuration = OneSignal.createConfiguration({
-    appKey: "MDk2ODU2YjUtNTlhYy00NTY5LWIwOWMtNDJlMjllYTMyOGQz",
-  });
-
-  const client = new OneSignal.DefaultApi(configuration);
-
-  const app = await client.getApp("57507296-ef2e-4355-990a-9f0b84790626");
-
-  const notification = new OneSignal.Notification();
-
-  notification.app_id = app.id as string;
-
-  notification.name = "test_notification_name";
-  notification.contents = {
-    en: "Gig'em Ags",
-  };
-
-  // required for Huawei
-  notification.headings = {
-    en: "Gig'em Ags",
-  };
-  await client.createNotification(notification);
+  // const configuration = OneSignal.createConfiguration({
+  //   appKey: "MDk2ODU2YjUtNTlhYy00NTY5LWIwOWMtNDJlMjllYTMyOGQz",
+  // });
+  //
+  // const client = new OneSignal.DefaultApi(configuration);
+  //
+  // const app = await client.getApp("57507296-ef2e-4355-990a-9f0b84790626");
+  //
+  // const notification = new OneSignal.Notification();
+  //
+  // notification.app_id = app.id as string;
+  //
+  // notification.name = "test_notification_name";
+  // notification.contents = {
+  //   en: "Gig'em Ags",
+  // };
+  //
+  // // required for Huawei
+  // notification.headings = {
+  //   en: "Gig'em Ags",
+  // };
+  // await client.createNotification(notification);
 
   return {
     statusCode: 200,
